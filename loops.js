5cd6641db99941900d72f3cd27501b9c6a061e78
Ajinung <isaacetor7@gmail.com>
2022-09-21 14:10:11 +0100

    working on loops in javascript

---
+++ loops.js
@@ -0,0 +1,42 @@
+//Do Loop
+let numbers = 0
+do {
+    // console.log(numbers)
+    numbers++
+} while (numbers <=10)
+
+//using Do loops to print out even numbers from 0 to 20
+let num = 0
+do{
+    // console.log(num)
+    num = num + 2;
+} while (num <= 20)
+
+//using conditions inside do while loop to print out even numbers 
+let newNum = 0;
+if(newNum % 2 === 0){
+    do{
+        // console.log(newNum)
+        newNum+=2
+    } while
+        (newNum<=20)
+    
+}
+
+//While Loop
+let digit = 1;
+let counter = 0;
+
+while(counter<10){
+    digit*= 2
+    // console.log(digit)
+    counter++
+}
+
+//Asg Isaac Etor
+//for loop
+let newCounter = 0
+for (let index = 1; newCounter <= 10; index*=2) {
+    console.log(index)
+    newCounter ++   
+}
